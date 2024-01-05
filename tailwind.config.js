004eac114c3a6ad1154608ed356591d5c640baf6
Filip <f@filip.world>
2024-01-05 01:03:57 -0500

    misc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md"],
+  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md", "../../assets/custom.css", "../../layouts/**/*.html"],
   theme: {
     extend: {},
   },
